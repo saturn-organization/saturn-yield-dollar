1c89b5aece3a2dcaf5f83d41de9e2f598a432993
SebMelendez01 <78228475+SebMelendez01@users.noreply.github.com>
2026-01-23 14:45:06 -0500

    issue I03: Update Interface to match contract (#59)

--- src/interfaces/IWithdrawalQueueERC721.sol
+++ src/interfaces/IWithdrawalQueueERC721.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 interface IWithdrawalQueueERC721 {
-    function addRequest(address owner, uint256 shares, uint256 minUsdatReceived) external;
+    function addRequest(address owner, uint256 shares, uint256 minUsdatReceived) external returns (uint256 tokenId);
 
     function claimAllFor(address user) external returns (uint256 totalAmount);
 
